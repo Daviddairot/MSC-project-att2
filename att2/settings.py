c51dbc0264933e348ae0625961613009ef8d53c3
DaviddairoT <dairotemitopedavid@gmail.com>
2024-12-07 18:28:31 +0100

    postgress

--- att2/settings.py
+++ att2/settings.py
@@ -76,11 +76,22 @@ WSGI_APPLICATION = 'att2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         # 'NAME': BASE_DIR / 'db.sqlite3',
+#         'NAME': os.path.join('/tmp', 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': os.path.join('/tmp', 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.jxxbpnsvvtkocghlnhrg',
+        'PASSWORD': 'Eu190304-1993',
+        'HOST': 'aws-0-eu-central-1.pooler.supabase.com',
+        'PORT': '6543',
     }
 }
 
